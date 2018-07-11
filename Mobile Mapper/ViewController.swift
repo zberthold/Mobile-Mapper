f06ce651afe2e09f5ce8a22387dec9784549edf9
Zoe Berthold <zoeberthold@zoes-macbook-pro.local>
2018-07-11 10:37:41 -0500

    added a location to the map

--- Mobile Mapper/ViewController.swift
+++ Mobile Mapper/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let northwesternAnnotation = MKPointAnnotation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let latitude = 42.055984
+        let longitude = -87.675171
+        let coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        northwesternAnnotation.coordinate = coordinate
+        northwesternAnnotation.title = "Northwestern University"
+        mapView.addAnnotation(northwesternAnnotation)
     }
 
     override func didReceiveMemoryWarning() {
